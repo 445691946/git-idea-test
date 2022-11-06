e40735448841b991746d5585d384162352837cb7
Ezreal <445691946@qq.com>
2022-11-06 17:57:02 +0800

    master fourth commit

--- src/main/java/com/atguigu/GitTest1.java
+++ src/main/java/com/atguigu/GitTest1.java
@@ -6,5 +6,6 @@ public class GitTest1 {
         System.out.println("fuck!fuck!fuck!");
         System.out.println("fuck!!!!!hot!fix!");
         System.out.println("fuck!!!!!hot!fix2!");
+        System.out.println("merge1");
     }
 }
